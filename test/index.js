18ab38dfb318982622d98140542e7fffb34dc729
Jeff Escalante <hello@jenius.me>
2016-03-31 18:46:14 -0400

    progress on the first test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,24 @@
-import example from '../lib/example'
 import test from 'ava'
+import Roots from 'roots-mini'
+import path from 'path'
+import Records from '..'
 
-test('example exports correctly', (t) => {
-  t.is(example, 'wow es6!')
+const fixturesPath = path.join(__dirname, 'fixtures')
+
+test.cb('loads data correctly', (t) => {
+  const projectPath = path.join(fixturesPath, 'data')
+  const project = new Roots({
+    root: projectPath,
+    entry: { main: [path.join(projectPath, 'app.js')] },
+    plugins: [new Records({ test: { foo: 'bar' } })]
+  })
+
+  project.on('error', t.end)
+  project.on('warning', t.end)
+  project.on('compile', (data) => {
+    // console.log(data)
+    t.end()
+  })
+
+  project.compile()
 })
