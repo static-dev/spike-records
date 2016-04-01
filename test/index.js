09e0eece5b327cf41925d3f04cbc777279672e8a
Jeff Escalante <hello@jenius.me>
2016-04-01 15:20:55 -0400

    tests passing for all data types

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 import test from 'ava'
 import Roots from 'roots-mini'
 import path from 'path'
+import fs from 'fs'
 import Records from '..'
+import rimraf from 'rimraf'
 
 const fixturesPath = path.join(__dirname, 'fixtures')
 
@@ -10,14 +12,53 @@ test.cb('loads data correctly', (t) => {
   const project = new Roots({
     root: projectPath,
     entry: { main: [path.join(projectPath, 'app.js')] },
-    plugins: [new Records({ test: { foo: 'bar' } })]
+    plugins: [new Records({ test: { data: { result: 'true' } } })]
   })
 
   project.on('error', t.end)
   project.on('warning', t.end)
   project.on('compile', (data) => {
-    // console.log(data)
-    t.end()
+    const out = fs.readFileSync(path.join(projectPath, 'public/index.html'), 'utf8')
+    t.is(out, 'true')
+    rimraf(path.join(projectPath, 'public'), t.end)
+  })
+
+  project.compile()
+})
+
+test.cb('loads a file correctly', (t) => {
+  const projectPath = path.join(fixturesPath, 'data')
+  const project = new Roots({
+    root: projectPath,
+    entry: { main: [path.join(projectPath, 'app.js')] },
+    plugins: [new Records({ test: { file: 'fixtures/testFile.json' } })]
+  })
+
+  project.on('error', t.end)
+  project.on('warning', t.end)
+  project.on('compile', (data) => {
+    const out = fs.readFileSync(path.join(projectPath, 'public/index.html'), 'utf8')
+    t.is(out, 'true')
+    rimraf(path.join(projectPath, 'public'), t.end)
+  })
+
+  project.compile()
+})
+
+test.cb('loads a url correctly', (t) => {
+  const projectPath = path.join(fixturesPath, 'data')
+  const project = new Roots({
+    root: projectPath,
+    entry: { main: [path.join(projectPath, 'app.js')] },
+    plugins: [new Records({ test: { url: 'http://api.bycarrot.com/staff' } })]
+  })
+
+  project.on('error', t.end)
+  project.on('warning', t.end)
+  project.on('compile', (data) => {
+    const out = fs.readFileSync(path.join(projectPath, 'public/index.html'), 'utf8')
+    t.is(out, 'true')
+    rimraf(path.join(projectPath, 'public'), t.end)
   })
 
   project.compile()
