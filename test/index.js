f74227a495ebcc6127a030056d6e8676675e5ad0
Jeff Escalante <hello@jenius.me>
2016-04-04 12:53:29 -0400

    transform implementation and test

--- test/index.js
+++ test/index.js
@@ -8,58 +8,87 @@ import rimraf from 'rimraf'
 const fixturesPath = path.join(__dirname, 'fixtures')
 
 test.cb('loads data correctly', (t) => {
-  const projectPath = path.join(fixturesPath, 'data')
-  const project = new Roots({
-    root: projectPath,
-    entry: { main: [path.join(projectPath, 'app.js')] },
-    plugins: [new Records({ test: { data: { result: 'true' } } })]
+  compileAndCheck({
+    test: t,
+    fixture: 'data',
+    config: { test: { data: { result: 'true' } } },
+    verify: (_, publicPath, cb) => {
+      const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
+      t.is(out, 'true')
+      cb()
+    }
   })
-
-  project.on('error', t.end)
-  project.on('warning', t.end)
-  project.on('compile', (data) => {
-    const out = fs.readFileSync(path.join(projectPath, 'public/index.html'), 'utf8')
-    t.is(out, 'true')
-    rimraf(path.join(projectPath, 'public'), t.end)
-  })
-
-  project.compile()
 })
 
 test.cb('loads a file correctly', (t) => {
-  const projectPath = path.join(fixturesPath, 'data')
-  const project = new Roots({
-    root: projectPath,
-    entry: { main: [path.join(projectPath, 'app.js')] },
-    plugins: [new Records({ test: { file: 'fixtures/testFile.json' } })]
+  compileAndCheck({
+    test: t,
+    fixture: 'data',
+    config: { test: { file: 'fixtures/testFile.json' } },
+    verify: (_, publicPath, cb) => {
+      const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
+      t.is(out, 'true')
+      cb()
+    }
   })
-
-  project.on('error', t.end)
-  project.on('warning', t.end)
-  project.on('compile', (data) => {
-    const out = fs.readFileSync(path.join(projectPath, 'public/index.html'), 'utf8')
-    t.is(out, 'true')
-    rimraf(path.join(projectPath, 'public'), t.end)
-  })
-
-  project.compile()
 })
 
 test.cb('loads a url correctly', (t) => {
-  const projectPath = path.join(fixturesPath, 'data')
+  compileAndCheck({
+    test: t,
+    fixture: 'data',
+    config: { test: { url: 'http://api.bycarrot.com/staff' } },
+    verify: (_, publicPath, cb) => {
+      const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
+      t.is(out, 'true')
+      cb()
+    }
+  })
+})
+
+test.cb('transform option works', (t) => {
+  compileAndCheck({
+    test: t,
+    fixture: 'data',
+    config: {
+      test: {
+        data: { result: true },
+        transform: (data) => { return { result: false } }
+      }
+    },
+    verify: (_, publicPath, cb) => {
+      const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
+      t.is(out, 'false')
+      cb()
+    }
+  })
+})
+
+//
+// Utilities
+//
+
+function configProject (fixturePath, recordsConfig) {
+  const projectPath = path.join(fixturesPath, fixturePath)
   const project = new Roots({
     root: projectPath,
     entry: { main: [path.join(projectPath, 'app.js')] },
-    plugins: [new Records({ test: { url: 'http://api.bycarrot.com/staff' } })]
+    plugins: [new Records(recordsConfig)]
   })
+  return [projectPath, project]
+}
 
+function compileProject (project, t, cb) {
   project.on('error', t.end)
   project.on('warning', t.end)
-  project.on('compile', (data) => {
-    const out = fs.readFileSync(path.join(projectPath, 'public/index.html'), 'utf8')
-    t.is(out, 'true')
-    rimraf(path.join(projectPath, 'public'), t.end)
-  })
-
+  project.on('compile', cb)
   project.compile()
-})
+}
+
+function compileAndCheck (opts) {
+  const [projectPath, project] = configProject(opts.fixture, opts.config)
+  const publicPath = path.join(projectPath, 'public')
+  compileProject(project, opts.test, (data) => {
+    opts.verify(data, publicPath, () => { rimraf(publicPath, opts.test.end) })
+  })
+}
