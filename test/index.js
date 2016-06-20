79d615c6a7051c53abf7a4f1c5a108ce024799d2
Jeff Escalante <hello@jenius.me>
2016-06-20 16:57:31 -0400

    Adjust features for latest version of spike (#18)

--- test/index.js
+++ test/index.js
@@ -4,53 +4,63 @@ const path = require('path')
 const fs = require('fs')
 const Records = require('..')
 const rimraf = require('rimraf')
+const exp = require('posthtml-exp')
 
 const fixturesPath = path.join(__dirname, 'fixtures')
 
 test.cb('loads data correctly', (t) => {
+  const locals = {}
   compileAndCheck({
     test: t,
     fixture: 'data',
-    config: { test: { data: { result: 'true' } } },
+    locals: locals,
+    config: { addDataTo: locals, test: { data: { result: 'true' } } },
     verify: (_, publicPath, cb) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
-      t.is(out, 'true')
+      t.is(out.trim(), '<p>true</p>')
       cb()
     }
   })
 })
 
 test.cb('loads a file correctly', (t) => {
+  const locals = {}
   compileAndCheck({
     test: t,
     fixture: 'data',
-    config: { test: { file: '../testFile.json' } },
+    locals: locals,
+    config: { addDataTo: locals, test: { file: '../testFile.json' } },
     verify: (_, publicPath, cb) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
-      t.is(out, 'true')
+      t.is(out.trim(), '<p>true</p>')
       cb()
     }
   })
 })
 
 test.cb('loads a url correctly', (t) => {
+  const locals = {}
   compileAndCheck({
     test: t,
     fixture: 'data',
-    config: { test: { url: 'http://api.bycarrot.com/staff' } },
+    locals: locals,
+    config: { addDataTo: locals, test: { url: 'http://api.bycarrot.com/staff' } },
     verify: (_, publicPath, cb) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
-      t.is(out, 'true')
+      t.is(out.trim(), '<p>true</p>')
       cb()
     }
   })
 })
 
 test.cb('transform option works', (t) => {
+  const locals = {}
   compileAndCheck({
     test: t,
     fixture: 'data',
+    locals: locals,
     config: {
+      addDataTo: locals,
       test: {
         data: { result: true },
         transform: (data) => { return { result: false } }
@@ -58,18 +68,20 @@ test.cb('transform option works', (t) => {
     },
     verify: (_, publicPath, cb) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
-      t.is(out, 'false')
+      t.is(out.trim(), '<p>false</p>')
       cb()
     }
   })
 })
 
 test.cb('single template errors with no "path" param', (t) => {
+  const locals = {}
   const {project} = configProject('template', {
+    addDataTo: locals,
     posts: {
       data: [{ title: 'wow' }, { title: 'amaze' }],
       template: {}
-    }
+    }, locals
   })
 
   project.on('warning', t.end)
@@ -83,11 +95,13 @@ test.cb('single template errors with no "path" param', (t) => {
 })
 
 test.cb('single template errors with no "output" param', (t) => {
+  const locals = {}
   const {project} = configProject('template', {
+    addDataTo: locals,
     posts: {
       data: [{ title: 'wow' }, { title: 'amaze' }],
       template: { path: 'foo' }
-    }
+    }, locals
   })
 
   project.on('warning', t.end)
@@ -101,11 +115,13 @@ test.cb('single template errors with no "output" param', (t) => {
 })
 
 test.cb('single template errors with non-array data', (t) => {
+  const locals = {}
   const {project} = configProject('template', {
+    addDataTo: locals,
     posts: {
       data: 'foo',
       template: { path: 'template.jade', output: () => { return 'wow.html' } }
-    }
+    }, locals
   })
 
   project.on('warning', t.end)
@@ -119,14 +135,17 @@ test.cb('single template errors with non-array data', (t) => {
 })
 
 test.cb('single template works with "path" and "template" params', (t) => {
+  const locals = {}
   compileAndCheck({
     test: t,
     fixture: 'template',
+    locals: locals,
     config: {
+      addDataTo: locals,
       posts: {
         data: [{ title: 'wow' }, { title: 'amaze' }],
         template: {
-          path: 'template.jade',
+          path: 'template.html',
           output: (item) => `posts/${item.title}.html`
         }
       }
@@ -135,24 +154,27 @@ test.cb('single template works with "path" and "template" params', (t) => {
       const index = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
       const wow = fs.readFileSync(path.join(publicPath, 'posts/wow.html'), 'utf8')
       const amaze = fs.readFileSync(path.join(publicPath, 'posts/amaze.html'), 'utf8')
-      t.is(index, '2')
-      t.is(wow, 'wow')
-      t.is(amaze, 'amaze')
+      t.is(index.trim(), '<p>2</p>')
+      t.is(wow.trim(), '<p>wow</p>')
+      t.is(amaze.trim(), '<p>amaze</p>')
       cb()
     }
   })
 })
 
 test.cb('single template works with "transform" param', (t) => {
+  const locals = {}
   compileAndCheck({
     test: t,
     fixture: 'template',
+    locals: locals,
     config: {
+      addDataTo: locals,
       posts: {
         data: { response: [{ title: 'wow' }, { title: 'amaze' }] },
         template: {
           transform: (data) => data.response,
-          path: 'template.jade',
+          path: 'template.html',
           output: (item) => `posts/${item.title}.html`
         }
       }
@@ -161,9 +183,9 @@ test.cb('single template works with "transform" param', (t) => {
       const index = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
       const wow = fs.readFileSync(path.join(publicPath, 'posts/wow.html'), 'utf8')
       const amaze = fs.readFileSync(path.join(publicPath, 'posts/amaze.html'), 'utf8')
-      t.is(index, '') // because the transform is not global
-      t.is(wow, 'wow')
-      t.is(amaze, 'amaze')
+      t.is(index.trim(), '<p>undefined</p>') // bc the transform is not global
+      t.is(wow.trim(), '<p>wow</p>')
+      t.is(amaze.trim(), '<p>amaze</p>')
       cb()
     }
   })
@@ -173,11 +195,13 @@ test.cb('single template works with "transform" param', (t) => {
 // Utilities
 //
 
-function configProject (fixturePath, recordsConfig) {
+function configProject (fixturePath, recordsConfig, locals) {
   const projectPath = path.join(fixturesPath, fixturePath)
   const project = new Spike({
     root: projectPath,
     entry: { main: [path.join(projectPath, 'app.js')] },
+    posthtml: { defaults: [exp({ locals })] },
+    ignore: ['template.html'],
     plugins: [new Records(recordsConfig)]
   })
   return { projectPath: projectPath, project: project }
@@ -191,7 +215,7 @@ function compileProject (project, t, cb) {
 }
 
 function compileAndCheck (opts) {
-  const {projectPath, project} = configProject(opts.fixture, opts.config)
+  const {projectPath, project} = configProject(opts.fixture, opts.config, opts.locals)
   const publicPath = path.join(projectPath, 'public')
   compileProject(project, opts.test, (data) => {
     opts.verify(data, publicPath, () => { rimraf(publicPath, opts.test.end) })
