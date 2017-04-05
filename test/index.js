849466190d095e8c240303b4e4f46b87af0ffda5
Jeff Escalante <jescalan@users.noreply.github.com>
2017-04-05 18:01:35 -0400

    Fix for spike@next (#51)
    * fix for spike@next
    
    * very thorough commenting of the source, small fixes
    
    * dep fix, scope issue fix
    
    * 0.5.0-0
    
    * update deps
    
    * GraphQL Implementation (#53)
    
    * graphql implementation
    
    * use graph.cool for test

--- test/index.js
+++ test/index.js
@@ -4,59 +4,73 @@ const path = require('path')
 const fs = require('fs')
 const Records = require('..')
 const rimraf = require('rimraf')
-const htmlStandards = require('spike-html-standards')
+const htmlStandards = require('reshape-standard')
 
 const fixturesPath = path.join(__dirname, 'fixtures')
 
-test.cb('loads data correctly', (t) => {
+test('loads data correctly', (t) => {
   const locals = {}
-  compileAndCheck({
-    test: t,
+  return compileAndCheck({
     fixture: 'data',
-    locals: locals,
+    locals,
     config: { addDataTo: locals, test: { data: { result: 'true' } } },
-    verify: (_, publicPath, cb) => {
+    verify: (_, publicPath) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
       t.is(out.trim(), '<p>true</p>')
-      cb()
     }
   })
 })
 
-test.cb('loads a file correctly', (t) => {
+test('loads a file correctly', (t) => {
   const locals = {}
-  compileAndCheck({
-    test: t,
+  return compileAndCheck({
     fixture: 'data',
     locals: locals,
     config: { addDataTo: locals, test: { file: '../testFile.json' } },
     verify: (_, publicPath, cb) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
       t.is(out.trim(), '<p>true</p>')
-      cb()
     }
   })
 })
 
-test.cb('loads a url correctly', (t) => {
+test('loads a url correctly', (t) => {
   const locals = {}
-  compileAndCheck({
-    test: t,
+  return compileAndCheck({
     fixture: 'data',
     locals: locals,
     config: { addDataTo: locals, test: { url: 'http://api.bycarrot.com/staff' } },
     verify: (_, publicPath, cb) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
       t.is(out.trim(), '<p>true</p>')
-      cb()
     }
   })
 })
 
-test.cb('transform option works', (t) => {
+test('loads a graphql endpoint correctly', (t) => {
   const locals = {}
-  compileAndCheck({
-    test: t,
+  return compileAndCheck({
+    fixture: 'graphql',
+    locals: locals,
+    config: {
+      addDataTo: locals,
+      test: {
+        graphql: {
+          url: 'https://api.graph.cool/simple/v1/cizz44m7pmezz016487cxed19',
+          query: '{ allPosts { description } }'
+        }
+      }
+    },
+    verify: (_, publicPath, cb) => {
+      const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
+      t.regex(out, /test/)
+    }
+  })
+})
+
+test('transform option works', (t) => {
+  const locals = {}
+  return compileAndCheck({
     fixture: 'data',
     locals: locals,
     config: {
@@ -69,7 +83,6 @@ test.cb('transform option works', (t) => {
     verify: (_, publicPath, cb) => {
       const out = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
       t.is(out.trim(), '<p>false</p>')
-      cb()
     }
   })
 })
@@ -137,10 +150,9 @@ test.cb('single template errors with non-array data', (t) => {
   project.compile()
 })
 
-test.cb('single template works with "path" and "template" params', (t) => {
+test('single template works with "path" and "template" params', (t) => {
   const locals = {}
-  compileAndCheck({
-    test: t,
+  return compileAndCheck({
     fixture: 'template',
     locals: locals,
     config: {
@@ -160,15 +172,13 @@ test.cb('single template works with "path" and "template" params', (t) => {
       t.is(index.trim(), '<p>2</p>')
       t.is(wow.trim(), '<p>wow</p>')
       t.is(amaze.trim(), '<p>amaze</p>')
-      cb()
     }
   })
 })
 
-test.cb('single template works with "transform" param', (t) => {
+test('single template works with "transform" param', (t) => {
   const locals = {}
-  compileAndCheck({
-    test: t,
+  return compileAndCheck({
     fixture: 'template',
     locals: locals,
     config: {
@@ -189,7 +199,6 @@ test.cb('single template works with "transform" param', (t) => {
       t.is(index.trim(), '<p>undefined</p>') // bc the transform is not global
       t.is(wow.trim(), '<p>wow</p>')
       t.is(amaze.trim(), '<p>amaze</p>')
-      cb()
     }
   })
 })
@@ -198,30 +207,66 @@ test.cb('single template works with "transform" param', (t) => {
 // Utilities
 //
 
+/**
+ * Given a fixture, records config, and locals, set up a spike project instance
+ * and return the instance and project path for compilation.
+ * @param  {String} fixturePath - path to the text fixture project
+ * @param  {Object} recordsConfig - config to be passed to record plugin
+ * @param  {Object} locals - locals to be passed to views
+ * @return {Object} projectPath (str) and project (Spike instance)
+ */
 function configProject (fixturePath, recordsConfig, locals) {
   const projectPath = path.join(fixturesPath, fixturePath)
   const project = new Spike({
     root: projectPath,
     entry: { main: path.join(projectPath, 'app.js') },
     matchers: { html: '*(**/)*.sgr' },
-    reshape: (ctx) => htmlStandards({ webpack: ctx, locals }),
+    reshape: htmlStandards({ locals: () => { return locals } }),
     ignore: ['template.sgr'],
     plugins: [new Records(recordsConfig)]
   })
   return { projectPath, project }
 }
 
-function compileProject (project, t, cb) {
-  project.on('error', t.end)
-  project.on('warning', t.end)
-  project.on('compile', cb)
-  project.compile()
+/**
+ * Given a spike project instance, compile it, and return a promise for the
+ * results.
+ * @param  {Spike} project - spike project instance
+ * @return {Promise} promise for a compiled project
+ */
+function compileProject (project) {
+  return new Promise((resolve, reject) => {
+    project.on('error', reject)
+    project.on('warning', reject)
+    project.on('compile', resolve)
+    project.compile()
+  })
 }
 
+/**
+ * Compile a spike project and offer a callback hook to run your tests on the
+ * results of the project.
+ * @param {Object} opts - configuration object
+ * @param {String} opts.fixture - name of the project folder inside /fixtures
+ * @param {Object} opts.locals - object to be passed to view engine
+ * @param {Object} opts.config - config object for records plugin
+ * @param {Function} opts.verify - callback for when the project has compiled,
+ * passes webpack compile result data and the project's public path
+ * @return {Promise} promise for completed compiled project
+ */
 function compileAndCheck (opts) {
   const {projectPath, project} = configProject(opts.fixture, opts.config, opts.locals)
   const publicPath = path.join(projectPath, 'public')
-  compileProject(project, opts.test, (data) => {
-    opts.verify(data, publicPath, () => { rimraf(publicPath, opts.test.end) })
+  return compileProject(project)
+    .then((data) => opts.verify(data, publicPath))
+    .then(() => rimrafPromise(publicPath))
+}
+
+function rimrafPromise (dir) {
+  return new Promise((resolve, reject) => {
+    rimraf(dir, (err) => {
+      if (err) return reject(err)
+      resolve()
+    })
   })
 }
